33827ae54dbce5ecc7007a896a61c3904ef7cd7a
JK Jung <jkjung13@gmail.com>
2017-09-17 04:21:44 +0000

    Add the example script for training with brainwash dataset

---
+++ train_brainwash.sh
@@ -0,0 +1,1 @@
+python ./tools/train_net.py --gpu 0 --solver ./models/brainwash/VGG16/faster_rcnn_end2end/solver.prototxt --weights ./data/imagenet_models/VGG16.v2.caffemodel --imdb brainwash_train --iters 70000 --cfg experiments/cfgs/brainwash.yml
