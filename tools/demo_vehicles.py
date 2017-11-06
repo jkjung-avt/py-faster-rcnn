dfcefba8e34d5d20fa984cd50ac586a5e9d01985
JK Jung <jkjung13@gmail.com>
2017-11-06 18:23:00 +0800

    Use soft nms to improve final detection results

--- tools/demo_vehicles.py
+++ tools/demo_vehicles.py
@@ -16,7 +16,7 @@ See README.md for installation instructions before running.
 import _init_paths
 from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
-from fast_rcnn.nms_wrapper import nms
+from fast_rcnn.nms_wrapper import soft_nms
 from utils.timer import Timer
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,6 +26,7 @@ import argparse
 import sys
 
 CLASSES = ('__background__', 'bicycle', 'car', 'motorcycle', 'bus', 'train', 'truck')
+COLORS = ((0,0,0), (0,76,153), (0,0,255), (51,153,255), (0,255,255), (0,204,0), (204,0,0))
 
 def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
@@ -62,11 +63,15 @@ def vis_detections(im, class_name, dets, thresh=0.5):
 def vis_detections_cv(im_name, im, dets_all, thresh=0.5):
     """Draw detected bounding boxes."""
     windowName = 'demo_vehicles'
-    inds = np.where(dets_all[:, -1] >= thresh)[0]
+    inds = np.where(dets_all[:, -2] >= thresh)[0]
     for i in inds:
         bbox = dets_all[i, :4]
-        score = dets_all[i, -1]
-        cv2.rectangle(im, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,0,255), 2)
+        score = dets_all[i, -2]
+        cls = int(dets_all[i, -1])
+        cv2.rectangle(im, (bbox[0],bbox[1]), (bbox[2],bbox[3]), COLORS[cls], 2)
+        txt = '{:s} {:.3f}'.format(CLASSES[cls], score)
+        cv2.putText(im, txt, (int(bbox[0])+1,int(bbox[1])-2), cv2.FONT_HERSHEY_PLAIN, 1.0, (32,32,32), 4, cv2.LINE_AA)
+        cv2.putText(im, txt, (int(bbox[0]),int(bbox[1])-2), cv2.FONT_HERSHEY_PLAIN, 1.0, (240,240,240), 1, cv2.LINE_AA)
     cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
     height, width, colors = im.shape
     cv2.resizeWindow(windowName, width, height)
@@ -87,22 +92,28 @@ def demo(net, image_name):
     timer = Timer()
     timer.tic()
     scores, boxes = im_detect(net, im)
-    timer.toc()
+    det_time = timer.toc(average=False)
 
+    #print('im_detect(): output {} boxes'.format(boxes.shape[0]))
+    timer.tic()
     dets_list = []
-    CONF_THRESH = 0.6
-    NMS_THRESH = 0.3
+    CONF_THRESH = 0.7
+    NMS_THRESH = 0.2
     for cls_ind, cls in enumerate(CLASSES[1:]):
         cls_ind += 1 # because we skipped background
         cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
         cls_scores = scores[:, cls_ind]
+        cls_indice = np.full(cls_scores.shape, cls_ind, dtype=np.float32)
         dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
-        keep = nms(dets, NMS_THRESH)
+        #keep = [i for i in range(dets.shape[0])]
+        keep = soft_nms(dets=dets, Nt=NMS_THRESH, method=1)
+        dets = np.hstack((dets, cls_indice[:, np.newaxis]))
         dets = dets[keep, :]
         dets_list.append(dets)
     dets_all = np.concatenate(dets_list, axis=0)
-    inds = np.where(dets_all[:, -1] >= CONF_THRESH)[0]
+    inds = np.where(dets_all[:, -2] >= CONF_THRESH)[0]
+    nms_time = timer.toc(average=False)
     print('Detection took {:.3f}s and found {:d} objects'.format(timer.total_time, len(inds)))
     vis_detections_cv(im_name, im, dets_all, thresh=CONF_THRESH)
 
@@ -124,6 +135,7 @@ if __name__ == '__main__':
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
     cfg.TEST.MAX_SIZE = 1920
     cfg.TEST.SCALES = (1080,)
+    cfg.TEST.RPN_POST_NMS_TOP_N = 1000
 
     args = parse_args()
 
@@ -136,8 +148,7 @@ if __name__ == '__main__':
         sys.exit('A valid network model has not been specified!')
 
     if not os.path.isfile(caffemodel):
-        raise IOError(('{:s} not found.\nDid you run ./data/script/'
-                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+        raise IOError('{:s} not found.'.format(caffemodel))
 
     if args.cpu_mode:
         caffe.set_mode_cpu()
